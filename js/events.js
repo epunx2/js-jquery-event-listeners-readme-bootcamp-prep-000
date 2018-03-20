98eda5ed9df608c0cef3af442a5eccdc939104e5
Eric Chrobak <eric@ericchrobak.com>
2018-03-20 01:35:48 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,8 +13,8 @@ function frameIt(){
 
 function pressIt(){
   $('#typing').on("keydown", function(key){
-    if(key.which ==){
-      
+    if(key.which == 71){
+      alert('g was pressed')
     }
   });
 }
